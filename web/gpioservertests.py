fcd0d18c60563123440e980fc5e38b433abe1869
Brett <Brett.Jackson@hscic.gov.uk>
2015-12-31 08:57:24 +0000

    Started adding unit tests...

---
+++ web/gpioservertests.py
@@ -0,0 +1,19 @@
+import unittest
+import gpioserver
+import os
+
+
+class gpioservertests(unittest.TestCase):
+
+#  def setUp(self):
+
+#  def tearDown(self):
+
+
+  def test_upper(self):
+    self.assertEqual(True, True)
+
+if __name__ == '__main__':
+  unittest.main()
+
+
